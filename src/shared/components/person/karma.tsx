0302e079c94343181ba89cf2e32ab0fc9c2a1ad7
karminski <code.karminski@outlook.com>
2024-08-29 01:38:47 +0800

    FINISH karma card

--- src/shared/components/person/karma.tsx
+++ src/shared/components/person/karma.tsx
@@ -18,21 +18,24 @@ export class Karma extends Component<KarmaProps, any> {
     const pv = this.props.pv;
     return (
       <>
-        <div>
-          <ul className="list-inline mb-2">
-            <li className="list-inline-item badge text-bg-light">
-              {I18NextService.i18n.t("number_of_posts", {
-                count: Number(pv.counts.post_count),
-                formattedCount: numToSI(pv.counts.post_count),
-              })}
-            </li>
-            <li className="list-inline-item badge text-bg-light">
-              {I18NextService.i18n.t("number_of_comments", {
-                count: Number(pv.counts.comment_count),
-                formattedCount: numToSI(pv.counts.comment_count),
-              })}
-            </li>
-          </ul>
+        <div className="card border-secondary mb-3">
+          <div className="card-body">
+            <h2 className="h5">{I18NextService.i18n.t("Karma")}</h2>
+            <ul className="list-inline mb-2">
+              <li className="list-inline-item badge text-bg-secondary">
+                {I18NextService.i18n.t("number_of_posts", {
+                  count: Number(pv.counts.post_count),
+                  formattedCount: numToSI(pv.counts.post_count),
+                })}
+              </li>
+              <li className="list-inline-item badge text-bg-secondary">
+                {I18NextService.i18n.t("number_of_comments", {
+                  count: Number(pv.counts.comment_count),
+                  formattedCount: numToSI(pv.counts.comment_count),
+                })}
+              </li>
+            </ul>
+          </div>
         </div>
       </>
     );
